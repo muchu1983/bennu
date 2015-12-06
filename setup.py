cb6d6dd9dc92a10bfa8a6fd711b664fb9989ee60
muchu hsu <muchu1983@gmail.com>
2015-12-06 13:54:50 +0800

    pypi

--- setup.py
+++ setup.py
@@ -5,3 +5,17 @@ This file is part of BSD license
 
 <https://opensource.org/licenses/BSD-3-Clause>
 """
+from setuptools import setup, find_packages
+
+setup(
+    name = "united",
+    version = "0.3.0",
+    keywords = ("united"),
+    description = "Image wiki system",
+    license = "BSD-3-Clause License",
+    install_requires = ["Pillow>=3.0.0"],
+    author = "MuChu Hsu",
+    author_email = "muchu1983@gmail.com",
+    packages = find_packages(),
+    platforms = "any",
+)
