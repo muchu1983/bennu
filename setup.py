3696bbb46b498e2faf2bee58a8fe405f7093fd9b
muchu hsu <muchu1983@gmail.com>
2015-12-08 13:57:59 +0800

    long

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ This file is part of BSD license
 """
 from setuptools import setup,find_packages
 
+with open("README.txt") as file:
+    long_description = file.read()
+
+
 setup(
     name = "united",
     version = "0.3.0.dev31",
@@ -16,6 +20,7 @@ setup(
     author_email = "muchu1983@gmail.com",
     license = "BSD 3-Clause License",
     url="https://github.com/muchu1983/united",
+    long_description=long_description,
     packages = find_packages(),
     include_package_data = True,
     install_requires = ["Pillow>=3.0.0"],
