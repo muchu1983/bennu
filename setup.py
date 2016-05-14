8ccb0e98ce127ee7798b6a7e56ce995aefcb5c3c
MuChu Hsu <muchu1983@gmail.com>
2016-05-15 02:38:41 +0800

    update version code

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.txt") as file:
 
 setup(
     name = "bennu",
-    version = "0.3.2.dev3",
+    version = "0.3.2.dev4",
     keywords = ["bennu", "utility", "muchu"],
     description = "muchu's utility module",
     author = "MuChu Hsu",
